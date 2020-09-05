37e3eae6fa514c983bf2048caa270c6c08b9040c
eddyv <vaismanedward@gmail.com>
2020-09-05 13:48:35 -0400

    added print of guild members

--- example_bot.py
+++ example_bot.py
@@ -6,10 +6,22 @@ client = discord.Client()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 @client.event
 async def on_message(message):
